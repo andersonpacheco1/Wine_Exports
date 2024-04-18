7f75ada44d6cdbe9c6199470d4355b20a60a633d
Stella <stellamaryane@gmail.com>
2024-04-18 13:28:07 -0300

    Added setup.py, added __main__ for executable, added __init__ for package #issue-1

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="wine-project",
+    version="0.1.0",
+    description="Wine dashboard project with Streamlit",
+    author="S. Costa",
+    packages=find_packages(),
+)
